71b87fa17c22b19fd537db622172a180b4f4247e
Maria Podolina <masapodolina@icloud.com>
2026-05-14 13:36:39 +0300

    add json properties for dto records, update pom dependencies

--- src/main/java/dev/moon/security/api_security/dto/CardRecord.java
+++ src/main/java/dev/moon/security/api_security/dto/CardRecord.java
@@ -1,5 +1,7 @@
 package dev.moon.security.api_security.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.UUID;
@@ -7,6 +9,7 @@ import java.util.UUID;
 public record CardRecord(
         UUID id,
         BigDecimal balance,
+        @JsonProperty("created_at")
         LocalDateTime createdAt
 ) {
 }
--- src/main/java/dev/moon/security/api_security/dto/CreateUserDto.java
+++ src/main/java/dev/moon/security/api_security/dto/CreateUserDto.java
@@ -1,10 +1,14 @@
 package dev.moon.security.api_security.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
 
 public record CreateUserDto(
-        @NotBlank String firstName,
-        @NotBlank String secondName,
+        @NotBlank @JsonProperty("first_name")
+        String firstName,
+        @NotBlank @JsonProperty("second_name")
+        String secondName,
+        @JsonProperty("middle_name")
         String middleName,
         @NotBlank String username,
         @NotBlank String password
